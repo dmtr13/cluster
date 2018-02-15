d58fdf2b7b4d649606d131fc705c6750170546b2
dmtr13 <dawirjowerdojo@gmail.com>
2018-02-15 22:25:55 +0100

    mcl prep

---
+++ bin/prepare_mcl.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import sys
+import pandas as pd
+
+print ("Preparing {} for MCL...".format(sys.argv[1]))
+"""
+Takes a normalised matrix and converts into a 3-column format acceptable
+for MCL.
+"""
+
+df = pd.read_csv(sys.argv[1], sep='\t', header=0, index_col=0)
+# Load the pandas DataFrame
+# iterate through each row/line, appending into a list (or maybe a file, if
+# it is more memory efficient) pairwise combination of the gene names
+# and on the third column the values from the matrix.

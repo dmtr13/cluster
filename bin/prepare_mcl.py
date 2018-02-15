aea9aeb60afd513f953d44b643593b92bff333d1
dmtr13 <dawirjowerdojo@gmail.com>
2018-02-15 22:38:15 +0100

    mcl prep

--- bin/prepare_mcl.py
+++ bin/prepare_mcl.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import sys
+import sys, os
 import pandas as pd
 
 print ("Preparing {} for MCL...".format(sys.argv[1]))
@@ -13,3 +13,9 @@ df = pd.read_csv(sys.argv[1], sep='\t', header=0, index_col=0)
 # iterate through each row/line, appending into a list (or maybe a file, if
 # it is more memory efficient) pairwise combination of the gene names
 # and on the third column the values from the matrix.
+
+
+filename, file_ext = os.path.splitext(sys.argv[1])
+outname = filename+"_MCLin"+file_ext
+
+print ("Done!")

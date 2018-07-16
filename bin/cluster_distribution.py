5374c5af3b55c1284c877b2cc801e30b1b2c8147
dmtr13 <dawirjowerdojo@gmail.com>
2018-07-16 17:50:10 +0200

    cluster distribution reworked

--- bin/cluster_distribution.py
+++ bin/cluster_distribution.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 ### (C) 2018 - Dimitri Wirjowerdojo #######
 ### https://github.com/dmtr13/cluster/ ####
-import sys, os, glob
+import glob
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from collections import Counter
-from operator import itemgetter
 
 bin = ['1-10', '11-50', '51-100', '101-250', '250-500', '501-1000',
         '1001-2500', '>2500']
@@ -35,10 +33,9 @@ metrics = ["Relative-Euclidean", "Euclidean",
             "Mass-Distance", "Manhattan"]
 threshold = [0.9, 0.925, 0.95, 0.96, 0.97, 0.98, 0.99, 0.995, 0.999]
 def clust_th(metrics):
-    print ("Metric: {}".format(metrics))
+    print ("Processing metric: {}".format(metrics))
     files = glob.glob("../Data/out*_{}*I20".format(metrics))
     files.sort()
-    # print (files)
     aggregate = [] #[threshold, [binnings]]
     for th, f in enumerate(files):
         size = []
@@ -47,23 +44,24 @@ def clust_th(metrics):
                 line = line.rstrip('\n').split('\t')
                 size.append(len(line))
         aggregate.append([threshold[th], binning(size)])
-    # print (aggregate)
+    return aggregate
 
-    print ("Plotting dist. of cluster size across different thresholds...")
+def plot(aggregate, metrics):
+    print ("Plotting...")
 
     N = len(bin)
     ind = np.arange(N)
     fig, ax = plt.subplots()
     width = 1/16
-    # p0 = ax.bar(ind-4*width, aggregate[0][1], width, color='b')
+    p0 = ax.bar(ind-4*width, aggregate[0][1], width, color='b')
     p1 = ax.bar(ind-3*width, aggregate[1][1], width, color='g')
     p2 = ax.bar(ind-2*width, aggregate[2][1], width, color='r')
     p3 = ax.bar(ind-width, aggregate[3][1], width, color='c')
     p4 = ax.bar(ind, aggregate[4][1], width, color='m')
     p5 = ax.bar(ind+width, aggregate[5][1], width, color='k')
-    # p6 = ax.bar(ind+2*width, aggregate[6][1], width, color='orange')
-    # p7 = ax.bar(ind+3*width, aggregate[7][1], width, color='steelblue')
-    # p8 = ax.bar(ind+4*width, aggregate[8][1], width, color='brown')
+    p6 = ax.bar(ind+2*width, aggregate[6][1], width, color='orange')
+    p7 = ax.bar(ind+3*width, aggregate[7][1], width, color='steelblue')
+    p8 = ax.bar(ind+4*width, aggregate[8][1], width, color='brown')
 
     title = 'Cluster_Size_Distribution_{}'.format(metrics)
     ax.set_title('Distribution of Cluster Size for {} Metric'.format(metrics))
@@ -74,60 +72,43 @@ def clust_th(metrics):
     ax.set_xlabel('Cluster Size')
     ax.autoscale_view()
     plt.savefig("../Data/{}.pdf".format(metrics))
-    # plt.show()
-
     return True
 
-for i in metrics:
-    clust_th(i)
-# clust_th("Euclidean")
-print ("Done")
+filepath = "../Data/MCL_Cluster_Distribution.xlsx"
+writer = pd.ExcelWriter(filepath, engine='xlsxwriter')
 
-# def clus_size(pathtofile):
-#     ### OUTFILE ################################################################
-#     filename_extension = os.path.basename(sys.argv[1])
-#     directory = os.path.dirname(sys.argv[1])
-#     fn, ext = os.path.splitext(filename_extension)
-#     fn = fn.replace('out', '').replace('.tsv.I20', '')
-#     fn = fn.split('_')
-#     fn = ' '.join(fn[2:4])
-#     # fn = fn.replace('out.HPA_processed_', '')
-#     # fn = fn.replace('_MCL.tsv.I20', '')
-#     print ("Processing {}...".format(fn))
-#     # outfile = directory+'/'+fn+'_KEGG'+'.tsv'
-#     ############################################################################
-#
-#     size = []
-#     with open(pathtofile, 'r') as fil:
-#         for line in fil:
-#             line = line.rstrip('\n').split('\t')
-#             size.append(len(line))
-#     # print (size)
-#     # plt.hist(size, bins=bin)
-#     # plt.ylim([0, 30])
-#     # plt.show()
-#     y = binning(size)
-#     x_shade = range(len(y))
-#     plt.figure()
-#     plt.bar(x_shade, y)
-#     plt.xticks(x_shade, bin, rotation=20)
-#     plt.ylabel('Counts')
-#     plt.xlabel('Size of Cluster')
-#     plt.tight_layout()
-#     plt.show()
-#     # size = [[x,size.count(x)] for x in set(size)]
-#     # size = sorted(size, key=itemgetter(0))
-#     # x, y = [], []
-#     # for z in size:
-#     #     x.append(str(z[0]))
-#     #     y.append(z[1])
-#     # x_shade = range(0, len(x)*4, 4)
-#     # plt.figure(figsize=(6,3))
-#     # plt.xticks(x_shade, x, rotation='vertical')
-#     # plt.bar(x_shade, y)
-#
-#     # for y in x:
-#     #     print (y)
-#
-# if __name__ == "__main__":
-#     print(clus_size(sys.argv[1]))
+from collections import defaultdict
+pt_dict = defaultdict(list)
+
+for z, i in enumerate(metrics):
+    aggregate = clust_th(i)
+    plot(aggregate, i)
+
+    ### per threshold ##########################################################
+    for th in aggregate:
+        if th[0] in pt_dict:
+            pt_dict[th[0]].append([th[1]])
+        else:
+            pt_dict[th[0]] = [th[1]]
+    ############################################################################
+
+    ### per metric #############################################################
+    if z == len(metrics)-1:
+        print ("\nAggregating cluster sizes per metric...")
+    pm = np.empty((len(threshold), len(bin)), dtype=int)
+    for row in range(len(threshold)):
+        pm[row, ] = np.array(aggregate[row][1])
+    pm = pd.DataFrame(pm, index=threshold, columns=bin)
+    pm.to_excel(writer, sheet_name=i)
+    ############################################################################
+
+print ("Aggregating cluster sizes per threshold...")
+for th in pt_dict:
+    thres = np.empty((len(metrics), len(bin)), dtype=int)
+    for row in range(len(metrics)):
+        thres[row, ] = np.array(pt_dict[th][row])
+    thres = pd.DataFrame(thres, index=metrics, columns=bin)
+    thres.to_excel(writer, sheet_name=str(th))
+
+writer.close()
+print ("Done!\n")
